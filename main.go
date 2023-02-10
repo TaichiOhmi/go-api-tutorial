c5bc520118791a26aebd05e1f8f7264c6b806bf4
TaichiOhmi <o6mi5.ta1@gmail.com>
2023-02-10 10:28:58 +0900

    add some function

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -18,6 +19,8 @@ type fishingResult struct {
 	Angler  string  `json:"angler"`
 	Results []catch `json:"results"`
 }
+
+var fishMaps = map[int]string{1: "アジ", 2: "タチウオ", 3: "イサキ", 4: "カサゴ", 5: "マダイ", 6: "アオリイカ", 7: "イシモチ", 8: "クロダイ", 10: "ヒイカ", 11: "アイナメ", 12: "サバ"}
 
 var fishingResults = []fishingResult{
 	{ID: "1", Angler: "Taichi", Results: []catch{{FishName: "アジ", Quantity: 30}}},
@@ -37,6 +40,75 @@ func fishingResultById(c *gin.Context) {
 		return
 	}
 	c.IndentedJSON(http.StatusOK, fRes)
+}
+
+func checkoutFishingResult(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+	result, err := getFishingResultById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Result not found"})
+		return
+	}
+	key, ok := c.GetQuery("key")
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing key query parameter"})
+		return
+	}
+	var ikey int
+	ikey, err = strconv.Atoi(key)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid key query parameter"})
+		return
+	}
+	for i, r := range result.Results {
+		if r.FishName == fishMaps[ikey] {
+			if r.Quantity <= 0 {
+				c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Result not available"})
+				return
+			} else {
+				result.Results[i].Quantity -= 1
+				c.IndentedJSON(http.StatusOK, result)
+				return
+			}
+		}
+	}
+	c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid key query parameter"})
+}
+
+func returnFishingResult(c *gin.Context) {
+	id, ok := c.GetQuery("id")
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing id query parameter"})
+		return
+	}
+	result, err := getFishingResultById(id)
+	if err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message": "Result not found"})
+		return
+	}
+	key, ok := c.GetQuery("key")
+	if !ok {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Missing key query parameter"})
+		return
+	}
+	var ikey int
+	ikey, err = strconv.Atoi(key)
+	if err != nil {
+		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid key query parameter"})
+		return
+	}
+	for i, r := range result.Results {
+		if r.FishName == fishMaps[ikey] {
+			result.Results[i].Quantity += 1
+			c.IndentedJSON(http.StatusOK, result)
+			return
+		}
+	}
+	c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "Invalid key query parameter"})
 }
 
 func getFishingResultById(id string) (*fishingResult, error) {
@@ -62,5 +134,7 @@ func main() {
 	router.GET("/fishing-results", getFishingResults)
 	router.GET("/fishing-result", fishingResultById)
 	router.POST("/fishing-results", createFishingResult)
+	router.PATCH("/fishing-results/checkout", checkoutFishingResult)
+	router.PATCH("/fishing-results/return", returnFishingResult)
 	router.Run("localhost:8080")
 }
